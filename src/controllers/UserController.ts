8feb8da743c900c227bb3fffd546a276ffc07a19
Toto Budi Trapsilo <toto2441999@gmail.com>
2023-04-03 08:41:15 +0700

    feat: add get, put and delete method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,23 +1,56 @@
 import { Request, Response } from "express";
 import { IController } from "./ControllerInterface";
 
+let data: any[] = [
+  { id: 1, name: "Jack", power: "375" },
+  { id: 2, name: "Queen", power: "450" },
+  { id: 3, name: "King", power: "500" },
+  { id: 4, name: "Ace", power: "500" },
+];
 class UserController {
   constructor() {}
 
   index(req: Request, res: Response): Response {
-    return res.send("index user is OK!");
+    return res.send(data);
   }
+
   create(req: Request, res: Response): Response {
-    return res.send(req.body);
+    const { name, power } = req.body;
+
+    const newHero = {
+      id: data.length + 1,
+      name: name,
+      power: power,
+    };
+
+    data.push(newHero);
+
+    return res.send("added!");
   }
+
   show(req: Request, res: Response): Response {
-    throw new Error("Method not implemented");
+    const { id } = req.params;
+
+    return res.send(data.find((x) => x.id == id));
   }
+
   update(req: Request, res: Response): Response {
-    throw new Error("Method not implemented");
+    const { id } = req.params;
+    const body = req.body;
+    let hero = data.find((x) => x.id == id);
+
+    hero.name = body.name;
+    hero.power = body.power;
+
+    return res.send("updated!");
   }
+
   delete(req: Request, res: Response): Response {
-    throw new Error("Method not implemented");
+    const { id } = req.params;
+
+    data = data.filter((x) => x.id != id);
+
+    return res.send("deleted!");
   }
 }
 
